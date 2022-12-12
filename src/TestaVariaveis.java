4f87ed71140c115d97f524bdc45b370fa385c8fc
renan-eng <renan3838@gmail.com>
2022-12-12 11:10:20 -0300

    tipo int e prinntln

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+public class TestaVariaveis {
+    public static void main(String[] args){
+        System.out.println("Novo teste");
+
+        int idade = 37;
+
+        System.out.println(idade);
+        idade = 10 +2;
+        idade = idade * 2 + 5;
+        System.out.println(idade);
+        System.out.println("Idade é " + idade);
+    }
+}
