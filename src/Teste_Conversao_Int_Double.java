aa3f4fde3c3f2b138df11df24de22ce827575b22
renan-eng <renan3838@gmail.com>
2022-12-12 12:05:13 -0300

    Castings (conversao) int e double

---
+++ src/Teste_Conversao_Int_Double.java
@@ -0,0 +1,9 @@
+public class Teste_Conversao_Int_Double {
+    public static void main(String[] args){
+        double salario = 1200.2;
+        // CASTING -
+        // conversão de DOUBLE PARA INT
+        int valor_convertido = (int) salario;
+        System.out.println(valor_convertido);
+    }
+}
