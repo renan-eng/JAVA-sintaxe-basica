4d94adb68a5f787a8b09d8f58e83202cca016f04
renan-eng <renan3838@gmail.com>
2022-12-13 12:35:10 -0300

    Loop while em java sintaxe

---
+++ src/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador <= 10){
+            System.out.println(contador);
+            // contador = contador + 1 ou contador += 1
+            contador++;
+        }
+        System.out.println(contador);
+    }
+}
