3b9302321aedd65f1dfcb429104f85b4dbd521c5
renan-eng <renan3838@gmail.com>
2022-12-13 11:18:12 -0300

    Condicional java

---
+++ src/Condicional.java
@@ -0,0 +1,17 @@
+public class Condicional {
+    public static void main(String[] args) {
+        System.out.println("testando condicionais");
+        int idade = 16;
+        int quantidadeDePessoa = 1;
+        if (idade >= 18) {
+            System.out.println("vc tem mais de 18 anos");
+            System.out.println("seja bem vindo");
+        } else {
+            if (quantidadeDePessoa > 1) {
+                System.out.println("vc pode entrar pq está acompanhado");
+            } else {
+                System.out.println("infelizmente vc não pode entrar");
+            }
+        }
+    }
+}
