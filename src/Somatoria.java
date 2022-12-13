d88c408c459fb9bbd201c64ad0f8209c5db8e56c
renan-eng <renan3838@gmail.com>
2022-12-13 12:34:56 -0300

    Exibe a somatoria de todos os numero inteiros até 10

---
+++ src/Somatoria.java
@@ -0,0 +1,11 @@
+public class Somatoria {
+    public static void main(String[] args) {
+        int contador = 0;
+        int somatoria = 0;
+        while (contador<=10){
+            System.out.println(somatoria);
+            contador++;
+            somatoria += contador;
+        }
+    }
+}
